42200770aad37f447daf1620cd6eee0ab4f7b074
yj-abe <m_abe@yoshidumi.co.jp>
2019-12-03 18:49:02 +0900

    デプロイの不具合を修正

--- plugin/src/main/kotlin/jp/cloudace/buildtypes/appengine/core/task/deploy/DeploySetupTask.kt
+++ plugin/src/main/kotlin/jp/cloudace/buildtypes/appengine/core/task/deploy/DeploySetupTask.kt
@@ -1,5 +1,7 @@
 package jp.cloudace.buildtypes.appengine.core.task.deploy
 
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
+import com.google.cloud.tools.gradle.appengine.core.DeployExtension
 import com.google.cloud.tools.gradle.appengine.standard.AppEngineStandardExtension
 import jp.cloudace.buildtypes.appengine.core.dsl.deploy.Deploy
 import org.gradle.api.DefaultTask
@@ -12,21 +14,27 @@ open class DeploySetupTask @Inject constructor(
     init {
         description = "Setup AppEngine deploy properties for ${deploy.name} build type."
         doFirst {
-            project.extensions.getByType(AppEngineStandardExtension::class.java)
-                .let { appengine ->
-                    appengine.deploy { originalDeploy ->
-                        originalDeploy.apply {
-                            projectId = deploy.projectId
-                            version = deploy.version
-                            promote = deploy.promote
-                            deploy.appEngineDirectory?.let { setAppEngineDirectory(it) }
-                            deploy.bucket?.let { bucket = it }
-                            deploy.imageUrl?.let { imageUrl = it }
-                            deploy.server?.let { server = it }
-                            deploy.stopPreviousVersion?.let { stopPreviousVersion = it }
-                        }
-                    }
+            project.extensions.findByType(AppEngineStandardExtension::class.java)
+                ?.let { appengine ->
+                    appengine.deploy { setup(it) }
                 }
+            project.extensions.findByType(AppEngineAppYamlExtension::class.java)
+                ?.let { appengine ->
+                    appengine.deploy { setup(it) }
+                }
+        }
+    }
+
+    private fun setup(originalDeploy: DeployExtension) {
+        originalDeploy.apply {
+            projectId = deploy.projectId
+            version = deploy.version
+            promote = deploy.promote
+            deploy.appEngineDirectory?.let { setAppEngineDirectory(it) }
+            deploy.bucket?.let { bucket = it }
+            deploy.imageUrl?.let { imageUrl = it }
+            deploy.server?.let { server = it }
+            deploy.stopPreviousVersion?.let { stopPreviousVersion = it }
         }
     }
 
